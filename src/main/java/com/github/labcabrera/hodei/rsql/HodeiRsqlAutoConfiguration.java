83b3c2f8286cc22ae7a0329fc54c821f7cbe472a
Luis Cabrera <luis.barragan.gfi@cnppartners.eu>
2020-10-20 19:51:31 +0200

    Auto-configuration bean update

--- src/main/java/com/github/labcabrera/hodei/rsql/HodeiRsqlAutoConfiguration.java
+++ src/main/java/com/github/labcabrera/hodei/rsql/HodeiRsqlAutoConfiguration.java
@@ -1,16 +1,25 @@
 package com.github.labcabrera.hodei.rsql;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.github.labcabrera.hodei.rsql.parser.RsqlParser;
+import com.github.labcabrera.hodei.rsql.service.RsqlCriteriaBuilder;
 
 @Configuration
 public class HodeiRsqlAutoConfiguration {
 
 	@Bean
+	@ConditionalOnMissingBean(RsqlParser.class)
 	RsqlParser rsqlParser() {
 		return new RsqlParser();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(RsqlCriteriaBuilder.class)
+	RsqlCriteriaBuilder rsqlCriteriaBuilder() {
+		return new RsqlCriteriaBuilder();
 	}
 
 }
